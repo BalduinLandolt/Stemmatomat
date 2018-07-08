13fa967ff5ec8f71b1de3cd8c7c283d46c27a30e
BalduinLandolt <balduin.landolt@unibas.ch>
2018-07-08 18:17:08 +0200

    initial commit

---
+++ src/Stemmatomat_V0_01.java
@@ -0,0 +1,11 @@
+public class Stemmatomat_V0_01 {
+
+    public static void main(String[] args){
+        System.out.println("Program Launched.");
+
+        //TODO run actual Application here.
+
+        System.out.println("Launcher Finished Successfully.");
+    }
+
+}
