12c4a983b773e1fc9d5dbfef46500bd43460dda2
baldu <baldu@DESKTOP-BVDFLTL.fritz.box>
2018-07-10 12:25:27 +0200

    Added Comment

--- src/main/java/Stemmatomat_V0_01.java
+++ src/main/java/Stemmatomat_V0_01.java
@@ -1,7 +1,18 @@
 import ch.unibas.landolt.balduin.stemmatomat.src.mainApplication.StemmatomatMain;
 
+/**
+ * A Runner Class to run stemmat-o-mat Version 0.01.
+ * 
+ * @author Balduin Landolt
+ *
+ */
 public class Stemmatomat_V0_01 {
 
+    /**
+     * Creates and runs an Instance of StemmatomatMain.
+     * 
+     * @param args ignored
+     */
     public static void main(String[] args){
         System.out.println("Program Launched.");
 
