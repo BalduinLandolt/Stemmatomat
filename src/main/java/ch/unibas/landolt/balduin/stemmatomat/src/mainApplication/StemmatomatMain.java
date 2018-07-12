6bf0e030ae9395ea06b2f1926e456d2324ebafeb
BalduinLandolt <balduin.landolt@unibas.ch>
2018-07-12 14:06:00 +0200

    implemented user preferences

--- src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/mainApplication/StemmatomatMain.java
+++ src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/mainApplication/StemmatomatMain.java
@@ -1,18 +1,26 @@
 package ch.unibas.landolt.balduin.stemmatomat.src.mainApplication;
 
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
 import ch.unibas.landolt.balduin.stemmatomat.src.gui.MainGUI;
 import ch.unibas.landolt.balduin.stemmatomat.src.util.Log;
+import ch.unibas.landolt.balduin.stemmatomat.src.util.Settings;
 
 public class StemmatomatMain {
 	
 	private MainGUI mainGUI;
+	private Preferences userPreferences;
 
 	public StemmatomatMain() {
 		super();
 		
 		Log.initialize();
+
+		Log.log("Running under Version: "+Runtime.class.getPackage().getImplementationVersion());
+		Log.log("Running under Version: "+System.getProperty("java.version"));
 		
-		//TODO get preferences
+		generatePreferences();
 		
 		//TODO other initialisations?
 		
@@ -20,20 +28,37 @@ public class StemmatomatMain {
 		mainGUI = new MainGUI(this);
 	}
 
+	private void generatePreferences() {
+		userPreferences = Preferences.userNodeForPackage(this.getClass());
+		try {
+			Log.log("Created Preferences: "+userPreferences.keys().length);
+		} catch (BackingStoreException e) {
+			Log.log("Failed to load Preferences.");
+			e.printStackTrace();
+		}
+	}
+
 	public void run() {
-		// TODO launch GUI
+		Settings.getSettings(userPreferences);
 		mainGUI.launch();
 	}
 
 	public void terminate() {
 		// TODO Auto-generated method stub
 		
+		savePreferences();
 		
 		Log.terminate();
 		System.exit(0);
 	}
+
+	private void savePreferences() {
+		// TODO Auto-generated method stub
+		
+
+		//updateSettings();
+		Settings.savePreferences(userPreferences);
+		
+	}
 	
-	
-	
-	//
 }
--- src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/util/Settings.java
+++ src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/util/Settings.java
@@ -3,21 +3,35 @@
  */
 package ch.unibas.landolt.balduin.stemmatomat.src.util;
 
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
 /**
  * @author Balduin Landolt
  *
  */
 public class Settings {
-	private static boolean openLogOnClose = true;
-	private static boolean askBeforeShutDown = false;
-	private static boolean autoOverwrite = false;
+	private static boolean openLogOnClose;
+	private static boolean askBeforeShutDown;
+	private static boolean autoOverwrite;
 	
 	public static boolean openLogOnClose() {
 		return openLogOnClose;
 	}
 	
-	public static void getSettings() {
-		//TODO load Settings from preferences
+	public static void getSettings(Preferences userPreferences) {
+		Log.log("loading Settings...");
+		
+		openLogOnClose = userPreferences.getBoolean("openLogOnClose", true);
+		Log.log("Got Setting: openLogOnClose="+openLogOnClose);
+		
+		askBeforeShutDown = userPreferences.getBoolean("askBeforeShutDown", false);
+		Log.log("Got Setting: askBeforeShutDown="+askBeforeShutDown);
+		
+		autoOverwrite = userPreferences.getBoolean("autoOverwrite", false);
+		Log.log("Got Setting: autoOverwrite="+autoOverwrite);
+		
+		Log.log("Done loading Settings.");
 	}
 
 	/**
@@ -32,5 +46,23 @@ public class Settings {
 	 */
 	public static boolean autoOverwrite() {
 		return autoOverwrite;
+	}
+
+	public static void savePreferences(Preferences userPreferences) {
+		Log.log("Saving Settings...");
+
+		userPreferences.putBoolean("openLogOnClose", openLogOnClose);
+		userPreferences.putBoolean("askBeforeShutDown", askBeforeShutDown);
+		userPreferences.putBoolean("autoOverwrite", autoOverwrite);
+		
+		int length = -1;
+		try {
+			length = userPreferences.keys().length;
+		} catch (BackingStoreException e) {
+			Log.log("Problem with preferences.");
+			e.printStackTrace();
+		}
+		
+		Log.log("Settings saved: "+length);
 	}
 }
