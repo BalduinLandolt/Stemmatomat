35bfa6ffd56d4717cbe22f75829562ba343fff88
BalduinLandolt <balduin.landolt@unibas.ch>
2018-07-30 17:59:26 +0200

    restructured Text class. should work

--- src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/util/Text.java
+++ src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/util/Text.java
@@ -2,6 +2,7 @@ package ch.unibas.landolt.balduin.stemmatomat.src.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Vector;
 
 public class Text {
@@ -9,17 +10,20 @@ public class Text {
 	private String identifier;
 	private String shelfmark;
 	
-	private String textContent;
+//	private String textContent;
+	private LinkedList<StringBuffer> segments;
 	
-	public Text(String id, String shelfM, String text) {
-		identifier = id;
-		shelfmark = shelfM;
-		textContent = text;
-	}
+//	public Text(String id, String shelfM, String text) {
+//		this(id, shelfM);
+//		textContent = text;
+//		
+//	}
 
 	public Text(String id, String sm) {
 		identifier=id;
 		shelfmark=sm;
+		segments = new LinkedList<>();
+		segments.add(new StringBuffer(""));
 	}
 
 	public String getIdentifier() {
@@ -38,45 +42,62 @@ public class Text {
 		this.shelfmark = shelfmark;
 	}
 
-	public String getTextContent() {
-		return textContent;
-	}
+//	public String getTextContent() {
+//		return textContent;
+//	}
+//
+//	public void setTextContent(String textContent) {
+//		this.textContent = textContent;
+//	}
 
-	public void setTextContent(String textContent) {
-		this.textContent = textContent;
-	}
-
-	public ArrayList<String> getList() {
-		ArrayList<String> v = new ArrayList<String>();
-		v.add(shelfmark);
-		v.add(identifier);
-		String[] ss = textContent.split("£");
-		for (String s: ss) {
-			v.add(s);
-		}
-		return v;
-	}
+//	public ArrayList<String> getList() {
+//		ArrayList<String> v = new ArrayList<String>();
+//		v.add(shelfmark);
+//		v.add(identifier);
+//		String[] ss = textContent.split("£");
+//		for (String s: ss) {
+//			v.add(s);
+//		}
+//		return v;
+//	}
 
 	public void appendText(String txt) {
-		if (textContent==null || textContent.isEmpty()) {
-			textContent = txt;
-		} else {
-			textContent += " " + txt;
-		}
+//		if (textContent==null || textContent.isEmpty()) {
+//			textContent = txt;
+//		} else {
+//			textContent += " " + txt;
+//		}
+		segments.getLast().append(txt);
 	}
 
 	public void appendSegmentation() {
-		if (textContent == null)
-			textContent = "";
-		textContent += "£";
-		textContent = textContent.replace("£ ", "£");
+//		if (textContent == null)
+//			textContent = "";
+//		textContent += "£";
+//		textContent = textContent.replace("£ ", "£");
+		segments.add(new StringBuffer(""));
 	}
 	
 	public String getTextWithPilcrow() {
-		String s = getTextContent();
+//		String s = getTextContent();
+//		String sep = "¶"+Log.lineSep;
+//		String res = s.replace("£", sep);
+//		return res;
+		StringBuffer res = new StringBuffer();
 		String sep = "¶"+Log.lineSep;
-		String res = s.replace("£", sep);
-		return res;
+		for (StringBuffer sb: segments) {
+			res.append(sb.toString());
+			res.append(sep);
+		}
+		return res.toString();
+	}
+
+	public int getLength() {
+		return segments.size();
+	}
+
+	public String getSegmentAt(int c) {
+		return segments.get(c).toString();
 	}
 	
 
--- src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/util/TextDisplayTableModel.java
+++ src/main/java/ch/unibas/landolt/balduin/stemmatomat/src/util/TextDisplayTableModel.java
@@ -7,45 +7,60 @@ import javax.swing.table.AbstractTableModel;
 @SuppressWarnings("serial")
 public class TextDisplayTableModel extends AbstractTableModel {
 	
-	private String[] head;
-	private String[][] data;
+//	private String[] head;
+//	private String[][] data;
 	private int cols = 0;
 	private int rows = 0;
+	private ArrayList<Text> texts;
 	
 
 	public TextDisplayTableModel(ArrayList<Text> texts) {
+		this.texts = texts;
 		int length = 0;
 		
 		for (Text t: texts) {
-			length = Math.max(length, t.getList().size());
+			length = Math.max(length, t.getLength());
 		}
+		
+		//TODO make sure every text is equally long
 		
 		rows = texts.size();
-		cols = length;
+		cols = length+2;
 		
 		
-		head = new String[cols];
-		data = new String[rows][cols];
-		
-		for (int r=0; r<rows; r++) {
-			Text t = texts.get(r);
-			ArrayList<String> tmp = t.getList();
-			for (int j=0; j<tmp.size();j++) {
-				data[r][j] = tmp.get(j);
-			}
-			length = Math.max(length, tmp.size());
-		}
-		
-		head[0] = "Shelf Mark";
-		head[1] = "ID";
-		for (int i = 2; i<length; i++) {
-			head[i] = "#"+(i-1);
-		}
+//		head = new String[cols];
+//		data = new String[rows][cols];
+//		
+//		for (int r=0; r<rows; r++) {
+//			Text t = texts.get(r);
+//			ArrayList<String> tmp = t.getList();
+//			for (int j=0; j<tmp.size();j++) {
+//				data[r][j] = tmp.get(j);
+//			}
+//			length = Math.max(length, tmp.size());
+//		}
+//		
+//		head[0] = "Shelf Mark";
+//		head[1] = "ID";
+//		for (int i = 2; i<length; i++) {
+//			head[i] = "#"+(i-1);
+//		}
 	}
 
 	@Override
 	public int getColumnCount() {
 		return cols;
+	}
+
+	@Override
+	public String getColumnName(int c) {
+		if (c == 0) {
+			return "Shelf Mark";
+		} else if (c == 1) {
+			return "ID";
+		} else {
+			return Integer.toString(c-1);
+		}
 	}
 
 	@Override
@@ -55,7 +70,15 @@ public class TextDisplayTableModel extends AbstractTableModel {
 
 	@Override
 	public Object getValueAt(int r, int c) {
-		return data[r][c];
+		//return data[r][c];
+		Text t = texts.get(r);
+		if (c == 0) {
+			return t.getShelfmark();
+		} else if (c == 1) {
+			return t.getIdentifier();
+		} else {
+			return t.getSegmentAt(c-2);
+		}
 	}
 
 	@Override
